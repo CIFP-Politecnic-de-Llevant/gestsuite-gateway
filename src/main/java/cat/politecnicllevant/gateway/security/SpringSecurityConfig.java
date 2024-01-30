88984a5e21c86cf45032ad86644c4f0d18d69273
Joan Galmés Riera <joan.galmes@gmail.com>
2024-01-30 15:02:56 +0100

    updated roles

--- src/main/java/cat/politecnicllevant/gateway/security/SpringSecurityConfig.java
+++ src/main/java/cat/politecnicllevant/gateway/security/SpringSecurityConfig.java
@@ -76,13 +76,17 @@ public class SpringSecurityConfig {
                 .pathMatchers("/api/core/sync/uploadfile").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name())
 
                 //Core - Usuaris
+                .pathMatchers("/api/core/usuaris/profile-by-email").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name(), RolDto.PROFESSOR.name())
+                .pathMatchers("/api/core/usuaris/profile-by-gestib-codi").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name(), RolDto.PROFESSOR.name())
                 .pathMatchers("/api/core/usuaris/**").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name())
                 .pathMatchers("/api/core/usuari/**").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name())
 
                 //Core - Curs
+                .pathMatchers("/api/core/curs/getByCodiGestib").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name(), RolDto.PROFESSOR.name())
                 .pathMatchers("/api/core/curs/**").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name())
 
                 //Core - Grups
+                .pathMatchers("/api/core/grup/llistatprofessorat").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name(), RolDto.PROFESSOR.name())
                 .pathMatchers("/api/core/grup/**").hasAnyAuthority(RolDto.ADMINISTRADOR.name(), RolDto.DIRECTOR.name(), RolDto.CAP_ESTUDIS.name())
 
                 //Core - Departament
